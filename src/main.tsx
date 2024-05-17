c758bb0a80b1eab6ff5c8112a1aced742d72d1c6
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-05-17 04:22:34 +0300

    project init structure & routing & tailwind configs - global styles

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./app";
+import { BrowserRouter } from "react-router-dom";
+ReactDom.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
