7449a00a9b78ad6b7b4873fc7ec3d84fa18dafeb
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-05-18 04:28:21 +0300

    sign up form styling & validation

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const signupValidation = z.object({
+  name: z.string().min(2, { message: "Too short" }),
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z
+    .string()
+    .email({ message: "Invalid email" })
+    .min(8, { message: "Too short" }),
+  password: z.string().min(8, { message: "Too short" }),
+});
