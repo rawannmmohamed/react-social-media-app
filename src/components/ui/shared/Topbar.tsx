70f96df719934427bc259fd6bd6084951ec9ea1f
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-05-28 02:51:51 +0300

    signout

--- src/components/ui/shared/Topbar.tsx
+++ src/components/ui/shared/Topbar.tsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../button";
-
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
 
 function Topbar() {
-    
-
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isSuccess) {
+      navigate('/');
+    }
+  });
   return (
     <div className="topbar">
       <div className="flex-between py-4 px-5">
@@ -12,7 +18,11 @@ function Topbar() {
           <img src="/assets/images/logo" alt="logo" width={130} height={325} />
         </Link>
         <div className="flex gap-4">
-          <Button variant="ghost" className="shad-button-ghost" onClick={()=> signOut}>
+          <Button
+            variant="ghost"
+            className="shad-button-ghost"
+            onClick={() => signOut}
+          >
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
         </div>
