2917ddc54196fe19f26d42c27887844219da9b5a
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-05-30 03:04:51 +0300

    private routes

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import { Route, Routes } from "react-router-dom";
 const app = () => {
@@ -17,11 +17,19 @@ const app = () => {
 
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+          <Route />
         </Route>
       </Routes>
-      <Toaster/>
+      <Toaster />
     </main>
-
   );
 };
 
